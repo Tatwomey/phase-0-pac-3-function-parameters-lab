c62157875693803fa2924a398e2c63da5b1e00e6
Trevor Twomey <trevor.a.twomey@gmail.com>
2022-12-04 21:29:54 -0500

    commit tree 4e5154fba7fff557e828d7617f55d5688709c734

---
+++ index.js
@@ -0,0 +1,19 @@
+function introduction(name1,Samip) {
+ if (name1 ==='Aki') return "Hi, my name is Aki."
+ else return "Hi, my name is Samip."
+ }
+
+function introductionWithLanguage(name,language){
+  if (name==='Aki') return "Hi, my name is Aki and I am learning to program in Ember.js."
+  if (name==='Samip')return "Hi, my name is Samip and I am learning to program in React."
+}
+
+function introductionWithLanguageOptional(JavaScript,name){
+  if (language==='Javascript') return "Hi, my name is Gracie and I am learning to program in Python."
+ else return "Hi, my name is Gracie and I am learning to program in JavaScript."
+}
+function introductionWithLanguageOptional(name, language){
+  if (name==='Gracie') return "Hi, my name is Gracie and I am learning to program in Python." 
+  if (name==='Gracie') return "Hi, my name is Gracie and I am learning to program in JavaScript."
+
+}
